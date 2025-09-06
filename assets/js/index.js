fe77c61d19048a8098acc0e0d9c0157de779feba
Muna Kandel <60501668+munakandel@users.noreply.github.com>
2025-09-05 21:45:41 -0500

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+
+// Smooth scrolling
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+  
